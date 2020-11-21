6fe1a31d7f3e7dfb6ef9a4a71d8cf634668a2c26
Crystal <crystalwatkins428@gmail.com>
2020-11-21 17:42:38 -0500

    worked on cors policy

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,39 +1,37 @@
 import axios from "axios";
 
-export default {
-    getOwnedBooks: function(id) {
-        console.log(id)
-        return axios.get(`/api/users/${id}`)
-    },
-    addOwnedBook: function(id, book) {
-        console.log(id)
-        return axios.put(`/api/users/added/${id}`, book)
-        //may have to change to 3001 to avoid cors policy!!!
-    },
-    deleteOwnedBook: function(id) {
-        console.log(id)
-        return axios.delete(`/api/books/${id}`)
-    },
-    requestToRent: function(id, user) {
-        console.log(id)
-        console.log(user)
-        return axios.put(`/api/books/${id}`, {userId: user});
-    },
-    getAllOwnedBooks: function() {
-        // console.log("hello")
-        return axios.get(`/api/books`)
-    },
-    bookRented: function(id, book) {
-        console.log(id)
-        console.log(book)
-        return axios.put(`/api/users/rent/${id}`, {bookId: book})
-    },
-    bookAvailable: function(id, book) {
-        console.log(id)
-        console.log(book)
-        return axios.put(`api/users/return/${id}`, {bookId: book})
-    },
-    // bookReturn: function(id) {
-    //     return axios.put(`api/books/${id}`, {bookId})
-    // }
+export function getOwnedBooks(id) {
+  console.log(id);
+  return axios.get(`/api/users/${id}`);
+};
+export function addOwnedBook(id) {
+  console.log(id);
+  return axios.put(`/api/users/added/${id}`, book);
+  //may have to change to 3001 to avoid cors policy!!!
+};
+export function deleteOwnedBook(id) {
+  console.log(id);
+  return axios.delete(`/api/books/${id}`);
+}
+export  function requestToRent(id, user) {
+  console.log(id);
+  console.log(user);
+  return axios.put(`/api/books/${id}`, { userId: user });
+}
+export  function getAllOwnedBooks() {
+  // console.log("hello")
+  return axios.get(`/api/books`);
+}
+export function bookRented(id, book) {
+  console.log(id);
+  console.log(book);
+  return axios.put(`/api/users/rent/${id}`, { bookId: book });
+}
+export function bookAvailable(id, book) {
+  console.log(id);
+  console.log(book);
+  return axios.put(`api/users/return/${id}`, { bookId: book });
+}
+export function bookReturn(id, bookId) {
+  return axios.put(`api/books/${id}`, { bookId });
 }
